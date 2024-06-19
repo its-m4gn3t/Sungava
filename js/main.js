1b64b22e1145191ddad357be67c68e2996fd929c
Prof. - Xadk3! <151055211+profxadke@users.noreply.github.com>
2024-06-19 23:51:37 +0545

    TODO: fix video thingy asap

--- js/main.js
+++ js/main.js
@@ -41,6 +41,33 @@ function registerHandler() {
 
       bodyHistory.push(document.body.innerHTML);
       document.querySelector('#app').innerHTML = markup;
+
+      
+      function customize(_, observer) {
+
+        let elem_visible = Boolean(document.querySelector('.course-3'));
+        
+        if (elem_visible && observer) {
+                let mainVid = document.querySelector('.main-video');
+
+        document.querySelectorAll('.course-3 .box .video video').forEach(vid =>{
+
+            vid.onclick = () =>{
+                let src = vid.getAttribute('src');
+                mainVid.classList.add('active');
+                mainVid.querySelector('video').src = src;
+            }
+
+        });
+
+        document.querySelector('#close-vid').onclick = () =>{
+            mainVid.classList.remove('active');
+        }
+            observer.disconnect();
+        }
+      }
+      new MutationObserver(customize).observe(document, { childList: true, subtree: true });
+
     })
   };
   document.getElementById("blog").onclick = (_event) => {
@@ -90,26 +117,26 @@ function registerHandler() {
 
       function customize(_, observer) {
 
-    let elem_visible = Boolean(document.querySelector('.course-3'));
-    
-    if (elem_visible && observer) {
-            let mainVid = document.querySelector('.main-video');
+        let elem_visible = Boolean(document.querySelector('.course-3'));
+        
+        if (elem_visible && observer) {
+                let mainVid = document.querySelector('.main-video');
 
-    document.querySelectorAll('.course-3 .box .video video').forEach(vid =>{
+        document.querySelectorAll('.course-3 .box .video video').forEach(vid =>{
 
-        vid.onclick = () =>{
-            let src = vid.getAttribute('src');
-            mainVid.classList.add('active');
-            mainVid.querySelector('video').src = src;
+            vid.onclick = () =>{
+                let src = vid.getAttribute('src');
+                mainVid.classList.add('active');
+                mainVid.querySelector('video').src = src;
+            }
+
+        });
+
+        document.querySelector('#close-vid').onclick = () =>{
+            mainVid.classList.remove('active');
         }
-
-    });
-
-    document.querySelector('#close-vid').onclick = () =>{
-        mainVid.classList.remove('active');
-    }
-        observer.disconnect();
-    }
+            observer.disconnect();
+        }
       }
       new MutationObserver(customize).observe(document, { childList: true, subtree: true });
     })
